6583778af5dcb1ba4d2c3eef417de83e521df02f
dgovorov7 <dg729219@ohio.edu>
2026-03-26 14:25:02 +0000

    install scripts

---
+++ install_lazygit.sh
@@ -0,0 +1,6 @@
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name"' | sed 's/.*"v\([^"]*\)".*/\1/')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+lazygit --version
+
